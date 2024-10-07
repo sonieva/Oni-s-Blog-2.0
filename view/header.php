9cc1104891550d118f879ec6eac38f49e02f792f
Santi Onieva <sonieva@re7systems.com>
2024-10-07 15:23:13 +0200

    Initial commit

---
+++ view/header.php
@@ -0,0 +1,60 @@
+<?
+// Santi Onieva
+
+session_start();
+
+$userLogged = isset($_SESSION['user_id']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <base href="/UF1/Pt04/">
+  <title>Oni's Blog 2.0</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link  href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="/assets/css/styles.css">
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+
+    <a class="navbar-brand" href="#">
+      <img src="assets/images/logo.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+      Oni's Blog 2.0
+    </a>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">
+            <i class="bi bi-house-door-fill"></i>
+            Inici
+          </a>
+        </li>
+
+        <li class="nav-item dropdown-center">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-person-fill"></i>
+            No loguejat
+          </a>
+          <ul class="dropdown-menu">
+            <? if ($userLogged): ?>
+              <li><a class="dropdown-item" href="#">Dashboard</a></li>
+              <li><a class="dropdown-item" href="#">Tancar sessió</a></li>
+            <? else: ?>
+            <li><a class="dropdown-item" href="#">Log in</a></li>
+            <li><a class="dropdown-item" href="#">Sign in</a></li>
+            <? endif; ?>
+          </ul>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
